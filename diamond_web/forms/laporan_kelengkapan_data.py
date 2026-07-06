d42d4eed7c7800c5b23d569d4a06978f13c667fe
zcracktz <jatrakman@gmail.com>
2026-07-06 12:09:48 +0700

    feat(ui): report modules templates and forms redesign
    - Redesign layouts for report listing pages (detail himpun olah data, kelengkapan data, metrik data eksternal, sla perekaman, transfer, etc.)
    - Optimize date-range select layouts and validation forms
    - Unify report filter inputs with fresh theme designs

--- diamond_web/forms/laporan_kelengkapan_data.py
+++ diamond_web/forms/laporan_kelengkapan_data.py
@@ -9,7 +9,7 @@ class LaporanKelengkapanDataFilterForm(forms.Form):
     """Form untuk filter Laporan Kelengkapan Data."""
 
     PERIODE_TYPE_CHOICES = [
-        ('', '-- Pilih Jenis Periode --'),
+        ('', 'Pilih Jenis Periode'),
         ('bulanan', 'Bulanan'),
         ('triwulanan', 'Triwulanan'),
         ('semester', 'Semester'),
@@ -17,7 +17,7 @@ class LaporanKelengkapanDataFilterForm(forms.Form):
     ]
 
     BULAN_CHOICES = [
-        ('', '-- Pilih Periode --'),
+        ('', 'Pilih Periode'),
         ('1', 'Januari'),
         ('2', 'Februari'),
         ('3', 'Maret'), 
@@ -33,7 +33,7 @@ class LaporanKelengkapanDataFilterForm(forms.Form):
     ]
 
     TRIWULAN_CHOICES = [
-        ('', '-- Pilih Periode --'),
+        ('', 'Pilih Periode'),
         ('1', 'Triwulan 1 (Jan - Mar)'),
         ('2', 'Triwulan 2 (Apr - Jun)'),        
         ('3', 'Triwulan 3 (Jul - Sep)'),
@@ -41,13 +41,13 @@ class LaporanKelengkapanDataFilterForm(forms.Form):
     ]
 
     SEMESTER_CHOICES = [
-        ('', '-- Pilih Periode --'),
+        ('', 'Pilih Periode'),
         ('1', 'Semester 1 (Jan - Jun)'),
         ('2', 'Semester 2 (Jul - Dec)'),
     ]
 
     TAHUNAN_CHOICES = [
-        ('', '-- Pilih Periode --'),
+        ('', 'Pilih Periode'),
         ('all', 'Seluruh Tahun'),
     ]   
 
@@ -74,7 +74,7 @@ class LaporanKelengkapanDataFilterForm(forms.Form):
 
     tahun = forms.ChoiceField(
         label='Tahun Transfer',
-        choices=[('', '-- Pilih Tahun --')],
+        choices=[('', 'Pilih Tahun')],
         widget=forms.Select(attrs={
             'class': 'form-select',
             'id': 'filter-tahun',
@@ -86,7 +86,7 @@ class LaporanKelengkapanDataFilterForm(forms.Form):
     def __init__(self, *args, years=None, **kwargs):
         super().__init__(*args, **kwargs)
         if years:
-            self.fields['tahun'].choices = [('', '-- Pilih Tahun --')] + [
+            self.fields['tahun'].choices = [('', 'Pilih Tahun')] + [
                 (str(year), str(year)) for year in years
             ]
 
